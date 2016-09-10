55cbd63f3bb5517f864356935aa2602c20edad2b
msun17 <msun17@studentmail.nait.ca>
2016-09-09 20:48:49 -0600

    Initial Website creation

---
+++ Repo Practice Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Repo_Practice_Website.Startup))]
+namespace Repo_Practice_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
